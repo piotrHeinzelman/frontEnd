b15be55a560cd3cf91d0450d35ab1bfb4125138a
marcin <71821642+MarcinPloch@users.noreply.github.com>
2020-10-30 11:57:47 +0100

    Update script.js

--- dedraeditor/script.js
+++ dedraeditor/script.js
@@ -1,5 +1,7 @@
 //alert ( "???" );
-const btn = document.querySelector('input[type="submit"]');
-btn.addEventListener('click', () => {
-  alert('OK');
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.querySelector('input[type="submit"]');
+    btn.addEventListener('click', () => {
+      alert('OK');
+    });
 });
