c2d97d92d9ff632bbbbae1da2549e25b61b21ccb
marcin <71821642+MarcinPloch@users.noreply.github.com>
2020-10-30 19:19:30 +0100

    Update script.js

--- dedraeditor/script.js
+++ dedraeditor/script.js
@@ -1,7 +1,34 @@
-//alert ( "???" );
-// document.addEventListener('DOMContentLoaded', () => {
-//     const btn = document.querySelector('input[type="submit"]');
-//     btn.addEventListener('click', () => {
-//       alert('OK');
-//     });
-// });
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.createElement('button');
+const btn1 = document.createElement('button');
+btn.classList.add('changeButton');
+btn1.classList.add('changeButton');
+btn.setAttribute('id', 'v_hangeButton');
+btn1.setAttribute('id', 'h_hangeButton');
+btn.innerText = '|| zamień dane';
+btn1.innerText = '––';
+
+const langForm = document.querySelector('form');
+langForm.setAttribute('action', '');
+langForm.after(btn);
+
+const changeContentVertical = () => {
+    const inputLang = document.querySelectorAll('form input.col3');
+    const inputPL = document.querySelectorAll('#gridPL input.col3');
+
+    for (let i = 1; i < inputLang.length; i++) {
+        inputLang[i].setAttribute("value", inputPL[i].getAttribute("value"));
+    }
+}
+
+const changeDescription = () => {
+    const description2 = document.querySelector('form textarea.description2');
+    const descrPL = document.querySelector('form .row_description span.description').innerHTML;
+    description2.innerHTML = descrPL;
+}
+
+btn.addEventListener('click', () => {
+    changeContentVertical();
+    changeDescription();
+});
+});
